ede53525039f53658f2beba65724206b8c7b3d6e
Ahmed Sharif <ahmed.sharif.bd@gmail.com>
2013-07-06 05:59:51 -0700

    uploading the correct solution

--- 10038-JollyJumpers/10038.c
+++ 10038-JollyJumpers/10038.c
@@ -6,10 +6,19 @@ int sum(int n)
 	return (n*(n+1))/2;
 }
 
+int compare_function(const void *a,const void *b) 
+{
+	int *x = (int *) a;
+	int *y = (int *) b;
+	return *x - *y;
+}
+
+
 int main()
 {
 
 	long n,i,required,previous,current,present;
+	long diff[3000], difference,flag;
 	while(scanf("%ld",&n)!=EOF)
 	{
 		previous=0;
@@ -20,14 +29,31 @@ int main()
 			scanf("%ld",&current);
 			if(i!=0)
 			{
-				present+=abs(current-previous);
+				difference=abs(current-previous);
+				diff[i-1]=difference;
+				present+=difference;
 			}
 			previous=current;
 		}
 		/* scanf("\n"); */
+		flag=1;
 		if(present==required)
 		{
-			printf("Jolly\n");
+			qsort(diff,n-1,sizeof(long),compare_function);
+			/*for(i=0;i<n-1;i++)
+				printf("%ld ",diff[i]); */
+			for(i=0;i<n-1;i++)
+			{
+				if(diff[i]!=(i+1))
+				{
+					flag=0;
+					break;
+				}
+			}
+			if(flag==1)	
+				printf("Jolly\n");
+			else
+				printf("Not jolly\n");
 		}
 		else
 		{
